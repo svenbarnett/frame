ee013d4ed7f8bba62daeb187847903aaf46392ce
3197544360@qq.com <3197544360@qq.com>
2019-10-11 16:06:15 +0800

    【A】初始化springboot工程结构

---
+++ frame-common/src/main/java/com/deepblue777/frame/config/AppConfig.java
@@ -0,0 +1,15 @@
+package com.deepblue777.frame.config;
+
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类
+ *
+ * @author pswen/3197544360@qq.com
+ * @date 2019/10/11 15:43
+ * @since 1.0
+ */
+@Configuration
+public class AppConfig {
+}
---
+++ frame-web/src/main/java/com/deepblue777/frame/App.java
@@ -0,0 +1,19 @@
+package com.deepblue777.frame;
+
+import com.deepblue777.frame.config.AppConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 框架主应用文件入口
+ *
+ * @author pswen/3197544360@qq.com
+ * @date 2019/10/11 15:40
+ * @since 1.0
+ */
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(AppConfig.class);
+  }
+}
