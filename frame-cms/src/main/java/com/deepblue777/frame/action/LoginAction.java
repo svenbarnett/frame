db8fc99b2c0a8d59bc4b712c7b4d2775b703396c
3197544360@qq.com <3197544360@qq.com>
2019-10-11 18:07:38 +0800

    【A】引入thymeleaf模板技术

--- frame-cms/src/main/java/com/deepblue777/frame/action/LoginAction.java
+++ frame-cms/src/main/java/com/deepblue777/frame/action/LoginAction.java
@@ -1,8 +1,10 @@
 package com.deepblue777.frame.action;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 登录相关控制器
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginAction {
 
   @RequestMapping(value = "/index", method = RequestMethod.GET)
-  public String index() {
-    return "index";
+  public ModelAndView index(Model model) {
+    model.addAttribute("userList","user");
+    model.addAttribute("title","用户管理");
+    return new ModelAndView("index","userModel",model);
   }
 }
