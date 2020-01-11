9803361267a4b6bdc8f7c1f00df3d23017c530de
svenbarnett <3197544360@qq.com>
2020-01-11 21:00:46 +0800

    【F】调整为tomcat启动

--- frame-web/src/main/java/com/deepblue777/frame/App.java
+++ frame-web/src/main/java/com/deepblue777/frame/App.java
@@ -1,8 +1,9 @@
 package com.deepblue777.frame;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -14,11 +15,26 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 
 @SpringBootApplication
+// 使用tkmapper的扫描组件
 @MapperScan(basePackages = {"com.deepblue777.frame.mapper"})
-@EnableAutoConfiguration
-//使用tkmapper的扫描组件
-public class App {
-  public static void main(String[] args) {
-    SpringApplication.run(App.class, args);
-  }
+public class App extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("" +
+                " _____  ______                        \n" +
+                "|  __ \\|  ____|                       \n" +
+                "| |  | | |__ _ __ __ _ _ __ ___   ___ \n" +
+                "| |  | |  __| '__/ _` | '_ ` _ \\ / _ \\\n" +
+                "| |__| | |  | | | (_| | | | | | |  __/\n" +
+                "|_____/|_|  |_|  \\__,_|_| |_| |_|\\___|");
+        System.out.println("starting ok！\n");
+
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(App.class);
+    }
 }
