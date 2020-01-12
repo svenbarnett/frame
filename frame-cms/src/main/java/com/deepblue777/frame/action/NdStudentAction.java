e230c05428df89a993432695a698fa3cc956338a
svenbarnett <3197544360@qq.com>
2020-01-12 10:31:34 +0800

    【F】调整身份证不显示问题

--- frame-cms/src/main/java/com/deepblue777/frame/action/NdStudentAction.java
+++ frame-cms/src/main/java/com/deepblue777/frame/action/NdStudentAction.java
@@ -74,8 +74,45 @@ public class NdStudentAction {
         }
 
         List<NdStudent> tables = ndStudentService.getTables(map, page, limit);
+        for (int i = 0; i < tables.size(); i++) {
+            NdStudent student = tables.get(i);
+            student.setIdcard(idMask(student.getIdcard(), 5, 4));
+        }
         int count = ndStudentService.getTablesCount(map);
         return JSON.toJSONString(new TableVO<>(count, tables));
+    }
+
+    /**
+     * 用户身份证号码的打码隐藏加星号加*
+     * <p>18位和非18位身份证处理均可成功处理</p>
+     * <p>参数异常直接返回null</p>
+     *
+     * @param idCardNum 身份证号码
+     * @param front     需要显示前几位
+     * @param end       需要显示末几位
+     * @return 处理完成的身份证
+     */
+    private String idMask(String idCardNum, int front, int end) {
+        //身份证不能为空
+        if (StringUtils.isBlank(idCardNum)) {
+            return null;
+        }
+        //需要截取的长度不能大于身份证号长度
+        if ((front + end) > idCardNum.length()) {
+            return null;
+        }
+        //需要截取的不能小于0
+        if (front < 0 || end < 0) {
+            return null;
+        }
+        //计算*的数量
+        int asteriskCount = idCardNum.length() - (front + end);
+        StringBuffer asteriskStr = new StringBuffer();
+        for (int i = 0; i < asteriskCount; i++) {
+            asteriskStr.append("*");
+        }
+        String regex = "(\\w{" + String.valueOf(front) + "})(\\w+)(\\w{" + String.valueOf(end) + "})";
+        return idCardNum.replaceAll(regex, "$1" + asteriskStr + "$3");
     }
 
     @PostMapping("/doadd")
